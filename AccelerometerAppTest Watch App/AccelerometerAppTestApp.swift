5897d440ef2c056f376491d86b71e7f50cb9d54a
Nicholas Schultz <darkmace22@gmail.com>
2023-02-13 14:01:49 -0600

    Initial Commit

---
+++ AccelerometerAppTest Watch App/AccelerometerAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccelerometerAppTestApp.swift
+//  AccelerometerAppTest Watch App
+//
+//  Created by Nicholas Schultz on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AccelerometerAppTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
